39c021b341148493dd2f940f11cd90947147a626
filipmrc <filip.maric300@gmail.com>
2016-05-09 19:22:53 +0200

    v0.05 - single leg IK + testing code, error checking

--- ker_matlab/kinematika.m
+++ ker_matlab/kinematika.m
@@ -1,24 +1,33 @@
 %%Generate and check direct kinematics
 
-syms a2 a3 d1 q1 q2 q3
-as = [0 a2 a3];
-ds = [d1 0 0];
-alphas = [pi 0 0];
-thetas = [-pi/2+q1 q2 q3];
+syms ab ah a1 a2 a3 dh d1 d4 q1 q2 q3
+as = [a1 a2 0 0];
+ds = [0 0 0 d4];
+alphas = [-pi/2 0 pi/2 0];
+thetas = [q1 q2 pi/2+q3 0];
 
-T = FK(as, ds, thetas, alphas);
-J = generateJacobian(T);
+T = simplify(FK(as, ds, thetas, alphas));
+J = simplify(generateJacobian(T));
 
-x = zeros(1,101);
-y = zeros(1,101);
-z = zeros(1,101);
-
-for i=1:101
-    T_ = subs(T, {'q1' 'q2' 'q3' 'a2' 'a3' 'd1'},{(i-1)*(2*pi/100) 0 0 1 1 0.2});
-    x(i) = T_(1,4);
-    y(i) = T_(2,4);
-    z(i) = T_(3,4);
+Tf = matlabFunction(T);
+e_max = 0;
+for i = 1:50
+    for j = 1:50
+        for k = 1:50
+            T_ = Tf(0.7, 7, 6, (i/50)*pi/3-pi/6 , (j/50)*pi-pi/2, (k/50)*pi-pi/2);
+            qs = IK_mex(double([T_(1,4) T_(2,4) T_(3,4)]));
+            e_min = [100 100 100];
+            for u = 1:16
+                tmp = Tf( 0.7, 7, 6, qs(1,u), qs(2,u), qs(3,u));               
+                e_min = min(e_min,abs([T_(1,4) T_(2,4) T_(3,4)] - tmp(1:3,4)'));
+            end
+            if e_min>[0.1 0.1 0.1]
+                disp([j k]);
+            end
+            e_max = max(e_max,e_min);
+        end
+    end
 end
 
-plot3(x,y,z);
-grid on;
+% plot3(x,y,z);
+% grid on;
